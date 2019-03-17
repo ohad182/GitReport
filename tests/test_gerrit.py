97a923e9c0160583714a321807d1533fe929fff2
ohadc <ohadc@marvell.com>
2019-03-17 12:27:37 +0200

    Fixed typo el to more meaningful classes
    Added support for report based on gerrit work
    Changed current providers to use the new model
    
    Change-Id: I8d6511831e333ff8b7b5e5d5171342e607107707

--- tests/test_gerrit.py
+++ tests/test_gerrit.py
@@ -5,7 +5,7 @@ from core.walker import Walker
 def test_gerrit_changes():
     from providers.gerrit import Gerrit
     config = GerritConfig(
-        root_directory="C:\\Git\\", author_name="user name",
+        root_directory="C:\\Git\\", author_name="User Name",
         author_email="user@company.com",
         remove_duplicates=True,
         url="http://gerrit.com",
@@ -18,10 +18,10 @@ def test_gerrit_changes():
 
 def test_gerrit_walker():
     config = GerritConfig(
-        root_directory="C:\\Git\\", author_name="Ohad Cohen",
-        author_email="ohadc@marvell.com",
+        root_directory="C:\\Git\\", author_name="User Name",
+        author_email="user@company.com",
         remove_duplicates=True,
-        url="http://vgitil10.il.marvell.com:8080",
+        url="http://gerrit.com",
         days_limit=7
     )
     walker = Walker(config=config)
